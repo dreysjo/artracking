c7844c9f5b2340a81d04f849970e04c56033fba4
dreysjo <audrey.josephine35@gmail.com>
2022-10-21 12:34:54 +0700

    new_update

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ def edit(id):
     return render_template('edit.html', customers=customers)
 
 if __name__ == "__main__":
-    # app.run(host='127.0.0.1', port=5000, debug=True, threaded=False)
-    app.run()
+    app.run(host='10.252.248.85', port=5000, debug=True, threaded=False)
+    # app.run()
